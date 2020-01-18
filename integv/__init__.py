8bcfc5c9f4d97083d2336a486f0ae619e60d5bb9
tetrau <tetrau01@gmail.com>
2020-01-18 21:57:22 +0800

    Add file integrity verifier for mp4 files.

--- integv/__init__.py
+++ integv/__init__.py
@@ -1,5 +1,7 @@
 import integv._file
 import weakref as _weakref
+import struct as _struct
+import functools as _functools
 
 
 class _IntegrityVerifierBase:
@@ -8,3 +10,26 @@ class _IntegrityVerifierBase:
     def __init_subclass__(cls, **kwargs):
         if hasattr(cls, "MIME"):
             cls._MIME_MAPPING[cls.MIME] = cls
+        if hasattr(cls, "verify"):
+            original_verify = cls.verify
+
+            @_functools.wraps(original_verify)
+            def wrapper(self, file):
+                file = _IntegrityVerifierBase._prepare_file(file)
+                return original_verify(self, file)
+
+            cls.verify = wrapper
+
+    @staticmethod
+    def _prepare_file(file):
+        if isinstance(file, _file.NormalizedFile):
+            return file
+        else:
+            return _file.NormalizedFile(file)
+
+    @staticmethod
+    def __peek_unpack_one(f, size, fmt):
+        return _struct.unpack(fmt, f.peek(size))[0]
+
+    def _peek_uint32_be(self, f):
+        return self.__peek_unpack_one(f, 4, ">L")
---
+++ integv/video.py
@@ -0,0 +1,25 @@
+from . import _IntegrityVerifierBase
+import integv._file as _file
+import struct as _struct
+
+
+class MP4IntegrityVerifier(_IntegrityVerifierBase):
+    MIME = "video/mp4"
+
+    def _check_chunk(self, f):
+        try:
+            chunk_size = self._peek_uint32_be(f)
+        except _struct.error:
+            return None
+        f.seek(chunk_size, _file.SEEK_CUR)
+        return chunk_size
+
+    def verify(self, file):
+        chunk_size_sum = 0
+        while True:
+            chunk_size = self._check_chunk(file)
+            if chunk_size is None:
+                break
+            else:
+                chunk_size_sum += chunk_size
+        return chunk_size_sum == len(file)
