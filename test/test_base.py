138a934277c62feb18736de5ab80f6be6089a8bd
tetrau <tetrau01@gmail.com>
2020-01-19 01:05:12 +0800

    Increase unit test coverage.

--- test/test_base.py
+++ test/test_base.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from integv import _IntegrityVerifierBase
+from integv._file import NormalizedFile
 
 
 class TestBase(TestCase):
@@ -7,8 +8,19 @@ class TestBase(TestCase):
         class TestVerifier(_IntegrityVerifierBase):
             MIME = "TEST"
 
-        self.test_verifier = TestVerifier
+            def verify(self, file):
+                return file
+
+        self.test_verifier = TestVerifier()
+
+    def test_prepare_file(self):
+        file_bytes = b"1234567890"
+        file = NormalizedFile(file_bytes)
+        self.assertIs(self.test_verifier.verify(file), file)
+        self.assertTrue(
+            isinstance(self.test_verifier.verify(file_bytes), NormalizedFile)
+        )
 
     def test_register(self):
-        self.assertIs(self.test_verifier,
+        self.assertIs(self.test_verifier.__class__,
                       _IntegrityVerifierBase._MIME_MAPPING["TEST"])
