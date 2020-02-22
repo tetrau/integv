a5021a37d477f183dde5d92a71ae548566156d8a
tetrau <tetrau01@gmail.com>
2020-02-23 01:19:18 +0800

    Update supported python version.
    Github workflow setup-python only supports python version >= 3.5.

--- setup.py
+++ setup.py
@@ -16,19 +16,16 @@ setup(
     description='A file integrity verifier based on the format of the file.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    python_requires='>=3.3',
+    python_requires='>=3.5',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
     ]
-
 )
